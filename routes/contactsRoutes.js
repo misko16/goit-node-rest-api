45d07f00dedbc3c69e5c7fcfe2f0e9eeb32e6df7
misko_16 <119707421+misko16@users.noreply.github.com>
2024-02-21 11:14:07 -0800

    firstTrying

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -2,31 +2,22 @@ const express = require("express");
 const router = express.Router();
 const contactsController = require("../controllers/contactsControllers");
 const validateBody = require("../helpers/validateBody");
-const {
-  createContactSchema,
-  updateContactSchema,
-} = require("../schemas/contactsSchemas");
+const {createContactSchema,updateContactSchema} = require("../schemas/contactsSchemas");
+const { registerRequest, loginRequest, logOutRequest, getCurrentUser } = require("../controllers/authControllers");
+const authMiddleware = require("../middlware/authMiddleware");
 
-// They are bouth working
-router.get("/", contactsController.getAllContacts);
-router.get("/:id", contactsController.getOneContact);
-// Also working
-router.post(
-  "/",
-  validateBody(createContactSchema),
-  contactsController.createContact
-);
-//This is also working 
-router.put(
-  "/:id",
-  validateBody(updateContactSchema),
-  contactsController.updateContact
-);
-// Also working
-router.delete("/:id", contactsController.deleteContact);
 
-// Working
-router.patch('/:id/favorite', contactsController.updateStatusContacts);
+router.get("/", authMiddleware, contactsController.getAllContacts);
+router.get("/:id", authMiddleware, contactsController.getOneContact);
+router.post("/", authMiddleware, validateBody(createContactSchema), contactsController.createContact);
+router.put("/:id", authMiddleware, validateBody(updateContactSchema), contactsController.updateContact);
+router.delete("/:id", authMiddleware, contactsController.deleteContact);
+router.patch('/:id/favorite', authMiddleware, contactsController.updateStatusContacts);
 
+//NEW
+router.post('/users/register', registerRequest);
+router.post("/users/login", loginRequest);
+router.post('/users/logout', authMiddleware, logOutRequest);
+router.get('/users/current', authMiddleware, getCurrentUser);
 
 module.exports = router;
---
+++ schemas/usersSchema.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const usersSchema = Joi.object({
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  }
+  );
+
+  module.exports = usersSchema;
