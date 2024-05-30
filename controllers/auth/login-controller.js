e199532c9b0da4672c7a537c113f50855c211605
misko_16 <119707421+misko16@users.noreply.github.com>
2024-05-30 04:42:27 -0800

    Error fix

--- controllers/auth/login-controller.js
+++ controllers/auth/login-controller.js
@@ -3,6 +3,7 @@ const { ctrlWrapper } = require("../../decorators");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const { HttpError } = require("../../helpers");
 const configPath = path.join(__dirname, "..", "..", ".env");
 require("dotenv").config({ path: configPath });
 
@@ -14,18 +15,17 @@ const login = async (req, res, next) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(401).json({ message: "Email or password is wrong" });
+      throw HttpError(401, "Email or password is wrong");
     }
 
-    // TODO виправити ситуацію з помилками
-    if(!user.verify){
-      throw res.status(404).json({message: "you are not verify your email"});
+    if (!user.verify) {
+      throw HttpError(404, "you are not verify your email");
     }
 
     const passwordCompare = await bcrypt.compare(password, user.password);
 
     if (!passwordCompare) {
-      return res.status(401).json({ message: "Email or password is wrong" });
+      throw HttpError(401, "Email or password is wrong");
     }
 
     const payload = {
